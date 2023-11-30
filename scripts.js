79c86bc9582fb0e50f84ded47c5df62eb975f59f
Willian Sena <williansena.webdesigner@gmail.com>
2023-11-29 23:22:02 -0300

    Atualizando arquivos ao projeto do github

---
+++ scripts.js
@@ -0,0 +1,58 @@
+const button = document.querySelector('.button-task')
+const input = document.querySelector('.input-task')
+const fullTask = document.querySelector('.list-task')
+let taskItem = []
+
+function adNewTask () {
+    taskItem.push({
+        task: input.value,
+        check: false
+    })
+
+    input.value = ''
+    viewTask()
+
+}
+
+function viewTask () {
+    let newItem = ''
+    
+    taskItem.forEach((item, position) => {
+       newItem = 
+       newItem + 
+       `
+           <li class="task ${item.check && "done"}">
+           <img src="./img/check.png" alt="check-todo" onclick="checkTask(${position})">
+           <p>${item.task}</p>
+           <img src="./img/remove.png" alt="remove-todo" onclick="removeItem(${position})">
+           </li>
+
+        `
+    } ) 
+    fullTask.innerHTML = newItem
+    localStorage.setItem('list', JSON.stringify(taskItem))
+
+}
+
+function removeItem(position) {
+    taskItem.splice(position, 1)
+    viewTask()
+}
+
+function checkTask(position) {
+    taskItem[position].check = !taskItem[position].check
+    viewTask()
+}
+
+function reloadTask(){
+    const taskLocalStore = localStorage.getItem("list")
+    if(taskLocalStore) {
+         taskItem = JSON.parse(taskLocalStore)
+    }
+
+    viewTask()
+
+} reloadTask()
+
+button.addEventListener('click', adNewTask)
+
